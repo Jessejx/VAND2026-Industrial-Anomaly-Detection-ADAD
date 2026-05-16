8231ebad3e0797a533869ae26562b4653a3791d1
Jesse <67528917+Jessejx@users.noreply.github.com>
2026-05-16 15:02:02 +0800

    Delete losses directory

--- losses/__init__.py
+++
@@ -1,8 +0,0 @@
-from .loss_cdo import CDOLoss, loss_cdo, normal_reconstruction_loss, residual_margin_loss
-
-__all__ = [
-    "CDOLoss",
-    "loss_cdo",
-    "normal_reconstruction_loss",
-    "residual_margin_loss",
-]
